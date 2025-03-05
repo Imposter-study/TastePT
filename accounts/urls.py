e361f275e5d4c8416be13dd2c4b27b64937de040
jeil <sjl01002@gmail.com>
2025-03-05 17:14:36 +0900

    ✨feat: signin
    -장고의 기본 기능을 사용한 세션로그인

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.SignUpAPIView.as_view(), name="signup"),
+    path("signin/", views.SignInAPIView.as_view(), name="signin"),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, get_user_model, login
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import SignUpSerializer
 
-# Create your views here.
+
+User = get_user_model()
+
+
+class SignUpAPIView(APIView):
+    def post(self, request):
+        serializer = SignUpSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SignInAPIView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        # 사용자 인증
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            return Response(
+                {"detail": "사용자 정보가 일치하지 않습니다."},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        # 로그인 처리 (세션에 사용자 정보 저장)
+        login(request, user)
+
+        return Response(
+            {
+                "detail": "로그인 성공",
+                "nickname": user.nickname,  # 로그인된 사용자 이름 반환 (선택 사항)
+            },
+            status=status.HTTP_200_OK,
+        )
