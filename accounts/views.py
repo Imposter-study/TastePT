07d9f2e4ca1ac2395a38f688cdfb848271c70450
jeil <sjl01002@gmail.com>
2025-03-05 16:23:11 +0900

    ✨feat: signin-session
    - 로그인 기능 추가
    - django의 기본 기능을 사용하여 세션방식으로 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate, get_user_model, login
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -17,3 +17,27 @@ class SignUpAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SignInAPIView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            return Response(
+                {"detail": "사용자 정보가 일치하지 않습니다."},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        login(request, user)
+
+        return Response(
+            {
+                "detail": "로그인 성공",
+                "nickname": user.nickname,
+            },
+            status=status.HTTP_200_OK,
+        )
