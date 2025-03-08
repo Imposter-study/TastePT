ce7e358378cea4dda690a6215a0ec31e3fc64d00
jeil <sjl01002@gmail.com>
2025-03-09 00:02:35 +0900

    í¹¹fix: password_confirm
    - 비밀번호를 변경할 때 변경할 비밀번호를 확인하도록 함.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -97,12 +97,35 @@ class ProfileUpdateSerializer(SignUpSerializer):
 class PasswordSerializer(serializers.Serializer):
     old_password = serializers.CharField(write_only=True, required=True)
     new_password = serializers.CharField(write_only=True, required=True)
+    new_password_confirm = serializers.CharField(write_only=True)
 
     def validate_old_password(self, value):
         user = self.context["request"].user
         if not user.check_password(value):
             raise serializers.ValidationError("현재 비밀번호가 일치하지 않습니다.")
         return value
+
+    def validate_new_password(self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError("비밀번호는 최소 8자 이상이어야 합니다.")
+        return value
+
+    def validate(self, data):
+        old_password = data.get("old_password")
+        new_password = data.get("new_password")
+        new_password_confirm = data.get("new_password_confirm")
+
+        if old_password == new_password:
+            raise serializers.ValidationError(
+                {"password_confirm": "이전 비밀번호와 현재 비밀번호가 같습니다."}
+            )
+
+        if new_password != new_password_confirm:
+            raise serializers.ValidationError(
+                {"password_confirm": "비밀번호가 일치하지 않습니다."}
+            )
+
+        return data
 
     def save(self, **kwargs):
         user = self.context["request"].user
