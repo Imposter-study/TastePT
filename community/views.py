1915b505eaaa55203d98a4326f02bc5102607081
yeonjae <pyj3740@gmail.com>
2025-03-10 16:51:56 +0900

    🩹 Fix : return post delete message
    - 게시글 삭제 후 삭제 완료 응답 추가

--- community/views.py
+++ community/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer
@@ -14,4 +16,21 @@ class PostViewSet(ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
+        # 게시글 생성 시 요청한 사용자를 게시글 작성자로 할당
         serializer.save(author=self.request.user)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        # 작성자만 삭제할 수 있도록 권한 검사
+        if instance.author != request.user:
+            return Response(
+                {"error": "삭제 권한이 없습니다."}, status=status.HTTP_403_FORBIDDEN
+            )
+
+        self.perform_destroy(instance)
+
+        # 삭제 후 커스텀 응답 반환
+        return Response(
+            {"detail": "게시글이 삭제되었습니다."}, status=status.HTTP_200_OK
+        )
