64735a4d750064e45fcd43b178a7aefc22c67033
Drills-hub <sdi6566@gmail.com>
2025-03-11 14:59:42 +0900

    ✨ Feat: Chatbot
    1.회원만 사용가능 2.관리자가 관리자 페이지에서  CSV파일을 등록. 3.임베딩을 파일당 한 번만 하고 하드에 저장

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .serializers import ChatbotSerializer
+from .chatbot import Chatbot_Run, VectorStoreManager
+from .models import Question
+
+User = get_user_model()
+
+
+class ChatbotAPIView(APIView):
+    def post(self, request):
+        if not request.user.is_authenticated:
+            return Response(
+                {"detail": "로그인을 해주세요."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        serializer = ChatbotSerializer(data=request.data)
+
+        if serializer.is_valid():
+            question = serializer.validated_data["question"]
+            created_by = request.user.nickname
+
+            Question.objects.create(
+                question=question, created_by=User.objects.get(nickname=created_by)
+            )
+
+            VectorStoreManager().add_file()
+            chatbot = Chatbot_Run()
+            response = chatbot.ask(question)
+
+            return Response({"answer": response}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
