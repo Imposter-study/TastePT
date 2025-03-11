d153f5292c9c0599b86b5e525c2cb51732ce206f
jeil <sjl01002@gmail.com>
2025-03-11 19:02:35 +0900

    fix: media url
    - 배포 후 사용할 media 파일의 url 수정

--- config/settings.py
+++ config/settings.py
@@ -153,9 +153,13 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-MEDIA_URL = "/media/"
+if DEBUG:
+    MEDIA_URL = "/media/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+else:
+    # 배포 후 s3와 연결 예정
+    pass
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
