41fd14e229f281df4136db4887f7d1a9bb7c25fc
yeonjae <pyj3740@gmail.com>
2025-03-22 13:30:40 +0900

    ♻️ Refactor: Refactor test data setup using setUpTestData
    -setUpTestData 사용해서 테스트 데이터 생성

--- community/tests.py
+++ community/tests.py
@@ -1,8 +1,7 @@
 from django.test import TestCase
-from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
 from rest_framework.test import APITestCase
-from sympy import content
 
 from .models import Post, Comment
 
@@ -10,30 +9,40 @@ User = get_user_model()
 
 
 # Create your tests here.
-class PostViewSetTest(APITestCase):
-    def setUp(self):
-        self.client = APIClient()
+class CommunityView(APITestCase):
 
+    @classmethod
+    def setUpTestData(cls):
         # 사용자 생성
-        self.user = User.objects.create_user(
+        cls.user = User.objects.create_user(
             email="test@test.com",
             password="pwpw1234",
             nickname="test",
         )
 
-        self.user2 = User.objects.create_user(
+        cls.user2 = User.objects.create_user(
             email="test2@test.com",
             password="pwpw1234",
             nickname="test2",
         )
 
         # 게시글 생성
-        self.post = Post.objects.create(
+        cls.post = Post.objects.create(
             title="test title",
             content="test content",
             thumbnail=None,
-            author=self.user,
+            author=cls.user,
         )
+
+        # 댓글 생성
+        cls.comment = Comment.objects.create(
+            content="test comment",
+            author=cls.user,
+            post=cls.post,
+        )
+
+    def setUp(self):
+        self.client = APIClient()
 
         # 로그인
         self.client.login(email="test@test.com", password="pwpw1234")
@@ -41,13 +50,13 @@ class PostViewSetTest(APITestCase):
         # community 앱의 base url 설정
         self.base_url = "/api/v1/community/"
 
-    # 게시글 목록 조회
+    # 게시글 목록 조회 : 200
     def test_get_post_list(self):
         self.client.logout()
         response = self.client.get(self.base_url)
         self.assertEqual(response.status_code, 200)
 
-    # 게시글 생성
+    # 게시글 생성 : 201
     def test_create_post(self):
         data = {
             "title": "test title",
@@ -56,7 +65,7 @@ class PostViewSetTest(APITestCase):
         response = self.client.post(self.base_url, data)
         self.assertEqual(response.status_code, 201)
 
-    # 로그인 없이 게시글 생성
+    # 로그인 없이 게시글 생성 : 403
     def test_create_post_without_authentication(self):
         self.client.logout()  # 로그아웃
         data = {
@@ -66,7 +75,7 @@ class PostViewSetTest(APITestCase):
         response = self.client.post(self.base_url, data)
         self.assertEqual(response.status_code, 403)
 
-    # 잘못된 데이터로 게시글 생성
+    # 잘못된 데이터로 게시글 생성 : 400
     def test_create_post_invalid_data(self):
         # 제목 누락
         data = {
@@ -75,19 +84,19 @@ class PostViewSetTest(APITestCase):
         response = self.client.post(self.base_url, data)
         self.assertEqual(response.status_code, 400)
 
-    # 게시글 상세 조회
+    # 게시글 상세 조회 : 200
     def test_get_post_detail(self):
         self.client.logout()
         response = self.client.get(f"{self.base_url}{self.post.id}/")
         self.assertEqual(response.status_code, 200)
 
-    # 없는 게시글 조회
+    # 없는 게시글 조회 : 404
     def test_get_post_detail_no_post(self):
         self.client.logout()
         response = self.client.get(f"{self.base_url}2314341234/")
         self.assertEqual(response.status_code, 404)
 
-    # 게시글 수정
+    # 게시글 수정 : 200
     def test_edit_post(self):
         # 수정할 데이터
         data = {
@@ -97,7 +106,7 @@ class PostViewSetTest(APITestCase):
         response = self.client.put(f"{self.base_url}{self.post.id}/", data)
         self.assertEqual(response.status_code, 200)
 
-    # 작성자가 아닌 유저가 게시글 수정
+    # 작성자가 아닌 유저가 게시글 수정 : 403
     def test_edit_post_not_author(self):
         self.client.logout()
         self.client.login(username="test2@test.com", password="pwpw1234")
@@ -109,12 +118,12 @@ class PostViewSetTest(APITestCase):
         response = self.client.put(f"{self.base_url}{self.post.id}/", data)
         self.assertEqual(response.status_code, 403)
 
-    # 게시글 삭제
+    # 게시글 삭제 : 204
     def test_delete_post(self):
         response = self.client.delete(f"{self.base_url}{self.post.id}/")
         self.assertEqual(response.status_code, 204)
 
-    # 작성자가 아닌 유저가 게시글 삭제
+    # 작성자가 아닌 유저가 게시글 삭제 : 403
     def test_delete_post_not_author(self):
         self.client.logout()
         self.client.login(username="test2@test.com", password="pwpw1234")
@@ -122,87 +131,62 @@ class PostViewSetTest(APITestCase):
         response = self.client.delete(f"{self.base_url}{self.post.id}/")
         self.assertEqual(response.status_code, 403)
 
-    # 댓글 생성
+    # 댓글 생성 : 201
     def test_create_comment(self):
         data = {"content": "test comment", "post": self.post}
         response = self.client.post(f"{self.base_url}{self.post.id}/comment/", data)
         self.assertEqual(response.status_code, 201)
 
-    # 로그인 없이 댓글 작성
+    # 로그인 없이 댓글 작성 : 403
     def test_create_comment_not_authenticated(self):
         self.client.logout()
         data = {"content": "test comment", "post": self.post}
         response = self.client.post(f"{self.base_url}{self.post.id}/comment/", data)
         self.assertEqual(response.status_code, 403)
 
+    # 대댓글 작성 : 201
+    def test_create_reply_comment(self):
+        data = {
+            "content": "reply comment",
+            "parent": self.comment.id,
+        }
+        response = self.client.post(f"{self.base_url}{self.post.id}/comment/", data)
+        self.assertEqual(response.status_code, 201)
 
-class ComentViewTest(APITestCase):
-    def setUp(self):
-        self.client = APIClient()
-
-        # 사용자 생성
-        self.user1 = User.objects.create_user(
-            email="test@test.com",
-            password="pwpw1234",
-            nickname="test",
-        )
-
-        self.user2 = User.objects.create_user(
-            email="test2@test.com",
-            password="pwpw1234",
-            nickname="test2",
-        )
-
-        self.post = Post.objects.create(
-            title="test title",
-            content="test content",
-            author=self.user1,
-        )
-
-        # 댓글 생성
-        self.comment = Comment.objects.create(
-            content="test comment",
-            author=self.user1,
-            post=self.post,
-        )
-
-        # 댓글의 baseURL
-        self.base_url = "/api/v1/community/comment/"
-
-    # 댓글 수정
+    # 댓글 수정 : 200
     def test_edit_comment(self):
         # 로그인
         self.client.login(username="test@test.com", password="pwpw1234")
         data = {"content": "edit comment"}
-        response = self.client.put(f"{self.base_url}{self.comment.id}/", data)
+        response = self.client.put(f"{self.base_url}comment/{self.comment.id}/", data)
         self.assertEqual(response.status_code, 200)
 
-    # 잘못된 데이터로 댓글 수정
+    # 잘못된 데이터로 댓글 수정 : 400
     def test_edit_content_invalid_data(self):
         # 로그인
         self.client.login(username="test@test.com", password="pwpw1234")
         data = {"content": ""}
-        response = self.client.put(f"{self.base_url}{self.comment.id}/", data)
+        response = self.client.put(f"{self.base_url}comment/{self.comment.id}/", data)
         self.assertEqual(response.status_code, 400)
 
-    # 작성자가 아닌 유저가 댓글 수정
+    # 작성자가 아닌 유저가 댓글 수정 : 403
     def test_edit_content_not_author(self):
         # 로그인
         self.client.login(username="test2@test.com", password="pwpw1234")
         data = {"content": "edit comment"}
-        response = self.client.put(f"{self.base_url}{self.comment.id}/", data)
+        response = self.client.put(f"{self.base_url}comment/{self.comment.id}/", data)
         self.assertEqual(response.status_code, 403)
 
-    # 댓글 삭제
+    # 댓글 삭제 : 204
     def test_delete_comment(self):
         # 로그인
         self.client.login(username="test@test.com", password="pwpw1234")
-        response = self.client.delete(f"{self.base_url}{self.comment.id}/")
+        response = self.client.delete(f"{self.base_url}comment/{self.comment.id}/")
         self.assertEqual(response.status_code, 204)
 
-    # 작성자가 아닌 유저가 댓글 삭제
+    # 작성자가 아닌 유저가 댓글 삭제 : 403
     def test_delete_content_not_author(self):
         # 로그인
         self.client.login(username="test2@test.com", password="pwpw1234")
-        response = self.client.delete(f"{self.base_url}{self.comment.id}/")
+        response = self.client.delete(f"{self.base_url}comment/{self.comment.id}/")
         self.assertEqual(response.status_code, 403)
