3dff91b36f6b5b704304c68ae89e3ceee61c5c79
jeil <sjl01002@gmail.com>
2025-03-23 21:45:24 +0900

    🩹 Fix: formmater
    - black 사용

--- config/settings.py
+++ config/settings.py
@@ -218,6 +218,7 @@ else:
     AWS_DEFAULT_ACL = "public-read"
     AWS_S3_OBJECT_PARAMETERS = {
         "CacheControl": "max-age=86400",
+        'ACL': 'public-read',
     }
 
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from chatbot.views import api 
+from chatbot.views import api
 from django.conf import settings
 from django.conf.urls.static import static
 
