414c9e1ce1558b2f989011c0bececaaf523e66d5
drill <sdi6566@gmail.com>
2025-04-18 16:53:06 +0900

    🐛 Fix: signals
    chatbot을 리팩토링 하는 과정에서 경로가 바뀐 부분에 맞춰 수정했습니다.

--- chatbot/signals.py
+++ chatbot/signals.py
@@ -1,10 +1,11 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Recipe
-from .chatbot import VectorStoreManager
+from .vectorstore import ChromaVectorStore
 
 @receiver(post_save, sender=Recipe)
 def embed_csv_on_upload(sender, instance, created, **kwargs):
     if created and instance.csv_file:
-        success = VectorStoreManager().add_file()
-        
+        ChromaVectorStore().add_file(file_path=instance.csv_file.path)
+        instance.is_embedded = True
+        instance.save()
